3ab9ae5c26dbd2128e9fc935b507282e8c99ccda
samiksha patre <sampatreo4@gmail.com>
2025-05-30 12:34:04 +0530

    new commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className='h1'>React App</h1>
+      <p className='p'>This is a React App with Simple CSS</p>
+      <button className='mybutton'>Click me</button>
+    </div>
+  );
+}
+
+export default App;

eb6cbd011f8293eaaf72b18730cc1ff3d47fb613
samiksha patre <sampatreo4@gmail.com>
2025-06-02 11:15:41 +0530

    New Button added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="container">
       <h1 className='h1'>React App</h1>
       <p className='p'>This is a React App with Simple CSS</p>
-      <button className='mybutton'>Click me</button>
+      <button className='mybutton'>Click</button>
+      <button className='mybutton'>Save</button>
+
     </div>
   );
 }
